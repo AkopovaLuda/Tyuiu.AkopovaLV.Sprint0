9d1e01940730a46cd1c4d67a694c734140843d89
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-09-16 23:44:48 +0300

    Task1

---
+++ Tyuiu.AkopovaLV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = " Luda ";
+        int age = 17;
+        Console.WriteLine("Hello, my name" + name + " mne " + age + " let!");
+        Console.ReadKey();
+    }
+}
