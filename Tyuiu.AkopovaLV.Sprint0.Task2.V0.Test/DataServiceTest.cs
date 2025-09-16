e7ce37ff404ea9f9a819d54c3d7c5823c28a5b40
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-09-17 02:08:34 +0300

    Task2

---
+++ Tyuiu.AkopovaLV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AkopovaLV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.AkopovaLV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Luda";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Hello, Luda", res);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.AkopovaLV.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Luda"));
+        Console.ReadKey();
+    }
+}
