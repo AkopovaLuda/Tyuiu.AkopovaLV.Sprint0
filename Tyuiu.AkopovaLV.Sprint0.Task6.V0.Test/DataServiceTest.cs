3940f490f755477baba9b0427d172e61b32bb7df
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-09-21 23:12:39 +0300

    Task6

---
+++ Tyuiu.AkopovaLV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.AkopovaLV.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.AkopovaLV.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var nambers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(nambers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var nambers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(nambers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckMultiplicationArrayValid()
+        {
+            var nambers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(nambers);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
